070534cba8ca9646ad957d729d84c4d97e0c8b67
alexnro <alexjandron99@gmail.com>
2019-03-20 20:42:24 +0100

    First test cases done

---
+++ src/main/java/org/lasencinas/Planeta.java
@@ -0,0 +1,54 @@
+package org.lasencinas;
+
+public enum Planeta {
+
+    MERCURY (3.303e+23, 2.4397e6),
+    VENUS   (4.869e+24, 6.0518e6),
+    EARTH   (5.976e+24, 6.37814e6),
+    MARS    (6.421e+23, 3.3972e6),
+    JUPITER (1.9e+27,   7.1492e7),
+    SATURN  (5.688e+26, 6.0268e7),
+    URANUS  (8.686e+25, 2.5559e7),
+    NEPTUNE (1.024e+26, 2.4746e7);
+
+    private double MASA = 0;
+    private double RADIO = 0;
+    private final double G = 6.67300E-11;
+
+    Planeta(double MASA, double RADIO) {
+        this.MASA = MASA;
+        this.RADIO = RADIO;
+    }
+
+    public double getMASA() {
+        return MASA;
+    }
+
+    public double getRADIO() {
+        return RADIO;
+    }
+
+    /*----------------------------------------Logica-------------------------------------------*/
+
+
+    public double getMasa(){
+        return this.getMASA();
+    }
+
+    public double getRadio() {
+        return this.getRADIO();
+    }
+
+    public double pesoSuperficie(double pesoHumano) {
+        return masaHumano(pesoHumano) * this.gravedadPlaneta();
+    }
+
+    public double gravedadPlaneta() {
+        return G * this.getMASA() / Math.pow(this.getRADIO(),2);
+    }
+
+    public double masaHumano(double pesoHumano) {
+        //tu_masa = tu_peso_en_la_Tierra / gravedad_superficial_tierra;
+        return pesoHumano / gravedadPlaneta();
+    }
+}
